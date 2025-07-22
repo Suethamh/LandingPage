6e98e2a1a7ee39904ef869594dcc2deefa45fd5f
Suethamh <matheushso2504@gmail.com>
2025-07-21 22:37:38 -0300

    Correcao do tituliSubtitulo

--- src/componentes/caixas/TituloSubtitulo/TituloSubtitulo.jsx
+++ src/componentes/caixas/TituloSubtitulo/TituloSubtitulo.jsx
@@ -3,8 +3,8 @@ import styles from "./TituloSubtitulo.module.css"
 function Titulosubtitulo(props) {
     return (
         <>
-            <h2>{props.titulo}</h2>
-            <h3>{props.subtitulo}</h3>
+            <h2 className={styles.titulo}>{props.titulo}</h2>
+            <h3 className={styles.subtitulo}>{props.subtitulo}</h3>
         </>
     );
 }
